5e25582b36e3aafe599877be744508ed32665cd8
kartik132000 <sujeetkumarsen37@gmail.com>
2025-04-03 15:45:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,13 @@
-import { useParams } from "react-router-dom";
 
-const items = [
-  { id: 1, name: "Item One", description: "This is item one." },
-  { id: 2, name: "Item Two", description: "This is item two." },
-];
+import React from "react";
+import './../styles/App.css';
 
-function ItemDetail() {
-  const { id } = useParams();
-  const item = items.find((item) => item.id === parseInt(id));
-
-  if (!item) return <h2>Item not found</h2>;
-
+const App = () => {
   return (
     <div>
-      <h1>{item.name}</h1>
-      <p>{item.description}</p>
+        {/* Do not remove the main div */}
     </div>
-  );
+  )
 }
 
-export default ItemDetail;
+export default App
