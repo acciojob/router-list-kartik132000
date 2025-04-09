4b3f1b03fe0ca2a80cfdd016516cc48c44d99bcf
kartik132000 <sujeetkumarsen37@gmail.com>
2025-04-09 14:16:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter,Link, Route, Routes } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+       <li> <Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link> </li>
+    </ul>
+    </header>
+  );
+};
+
+
+const Home = () => {
+  return (
+    <h1>
+     Welcome to my website!
+    </h1>  
+  );
+};
+const About = () => {
+  return (
+    <div>
+    <h1>
+     About
+    </h1>
+    <p>This is a sample React Router program.</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+   <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
