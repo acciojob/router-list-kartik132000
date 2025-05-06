157f3f9474cf7531856a15a4994b054fcc920122
kartik132000 <sujeetkumarsen37@gmail.com>
2025-05-06 15:42:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import './../styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ItemList from './ItemList';
 import ItemDetail from './ItemDetail';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<ItemList />} />
-                <Route path="/items/:id" element={<ItemDetail />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route path="/items/:id" element={<ItemDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
